53c5b13c15729c667a9acc260d9dd513c6d75e00
Fayeur9 <baptiste.freminet@gmail.com>
2025-02-13 12:08:08 +0100

    ajout du code js pour les datatables pour les logs

--- src/view/footer.php
+++ src/view/footer.php
@@ -1,8 +1,11 @@
 
         </main>
         <footer>
-            <script src="style.js?v=<?php echo time(); ?>"></script>
-            <script src="bootstrap.bundle.min.js"></script>
+            <script src="styles/js/jquery-3.7.1.js?v=<?php echo time();?>" ></script>
+            <script src="styles/js/dataTables.js?v=<?php echo time();?>" ></script>
+            <script src="styles/js/dataTables.bootstrap5.js?v=<?php echo time();?>" ></script>
+            <script src="styles/js/bootstrap.bundle.min.js?v=<?php echo time();?>" ></script>
+            <script src="styles/js/style.js?v=<?php echo time(); ?>"></script>
         </footer>
     </body>
 </html>
