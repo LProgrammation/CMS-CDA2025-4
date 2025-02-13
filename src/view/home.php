03f1a0311374e3db51ba90aa561f3002f2743354
Maureen <locationvimloc@gmail.com>
2025-02-13 12:39:04 +0100

    ajout de la page d'accueil avec classes  bootstrap

--- src/view/home.php
+++ src/view/home.php
@@ -1,5 +1,23 @@
-<body>
 <!-- Insert the home page content here -->
- <h1> Example of H1 content </h1>
+<?php include 'includes/header.html'; ?>
 
-</body>
+
+<h1>Page d'accueil </h1>
+
+
+    <!-- Section Voir les sites -->
+    <section class="container mt-5">
+        <h2 class="mb-3">Voir les sites</h2>
+
+       
+
+        <div class="row">
+        <? foreach ($sites as $site): ?>
+            <a href=""><div class="col-md-4">
+                <div class="p-3 bg-light border rounded"><?= $site->nom ."<br>". $site->utilisateur ?></div>
+            </div></a>
+        <? endforeach; ?>
+        </div>
+    </section>
+
+    <?php include 'includes/footer.html'; ?>
