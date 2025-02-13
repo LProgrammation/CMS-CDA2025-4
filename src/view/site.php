cac9f8f90223016bf9678014ce326c29d2b42379
Maureen <locationvimloc@gmail.com>
2025-02-13 16:51:41 +0100

    gvue page d'accueil : suppression head html

--- src/view/site.php
+++ src/view/site.php
@@ -1,12 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
-    <title>Document</title>
-</head>
-<body>
+
 
 
 <!-- Rappel : si le user est connecté, lui afficher ses sites dans une rubriques "mes sites", où il pourra supprimer, modifier et ajouter des pages/sites -->
@@ -30,8 +22,6 @@
     </div>
 
 </div> 
-</body>
-</html>
 
 
 
